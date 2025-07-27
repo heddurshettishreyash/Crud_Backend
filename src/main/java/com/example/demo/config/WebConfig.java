9a25a6fac97a6768432d18e8127b471398167a8d
heddurshettishreyash <heddurshettishreyash@gmail.com>
2025-07-27 20:09:22 +0530

    changed Web config

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -11,9 +11,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedOrigins("*")
+                .allowedOrigins("https://crud-backend-lfj2.onrender.com")
                 .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
