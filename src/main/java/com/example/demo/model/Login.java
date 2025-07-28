e9f5a41c0198838ae9b278ec0617da7d31f508a7
heddurshettishreyash <heddurshettishreyash@gmail.com>
2025-07-28 21:57:34 +0530

    changed config for testing 2

--- src/main/java/com/example/demo/model/Login.java
+++ src/main/java/com/example/demo/model/Login.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -10,6 +12,7 @@ import jakarta.validation.constraints.Size;
 
 public class Login {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @NotNull
 
